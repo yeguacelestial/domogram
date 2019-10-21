59c89c26133bb442b6cb5905224a087f63517924
Yegua Celestial <rexcolt@localhost.localdomain>
2019-10-20 22:26:52 -0400

    Created user and pin fields on db

--- login.py
+++ login.py
@@ -2,4 +2,10 @@ import sqlite3
 
 conexion = sqlite3.connect("usuarios.db")
 
+cursor = conexion.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS login" \
+    "(usuario VARCHAR(100), pin INTEGER)")
+
+conexion.commit()
 conexion.close()
