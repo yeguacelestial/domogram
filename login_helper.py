08d03c64535ba128eddb9666c1199854ee15075d
Yegua Celestial <rexcolt@localhost.localdomain>
2019-10-22 22:55:11 -0400

    Modified login helper and db refactor

--- login_helper.py
+++ login_helper.py
@@ -1,12 +1,44 @@
 import sqlite3
+import telepot
+import sys
+import os
+import time
+from dotenv import *
 
-class DBlogin:
+load_dotenv()
+
+token_admin = os.getenv('TOKEN_ADMIN')
+admin_bot = telepot.Bot(token_admin)
+
+def gestion(msg):
+    nombre = msg['from']['first_name']
+    user_id = str(msg['from']['id'])
+    tipo_contenido, tipo_chat, id_chat = telepot.glance(msg)
+
+    if (tipo_contenido == 'text'):
+        comando = msg['text']
+        print("Comando: %s" % comando)
+
+        if comando == '/start':
+            admin_bot.sendMessage(id_chat, "Hola, " + nombre + "! Tu id es: " + user_id)
+        
+        else:
+            admin_bot.sendMessage(id_chat, "Comando no reconocido.")
+
+    else:
+        admin_bot.sendMessage(id_chat, 'Soy listo, pero no más que tu. :-c')
+
+    #DB
     conexion = sqlite3.connect("usuarios.db")
-
     cursor = conexion.cursor()
 
     cursor.execute("CREATE TABLE IF NOT EXISTS login" \
-        "(usuario VARCHAR(100), pin INTEGER)")
+        "(nombre VARCHAR(100), pin INTEGER)")
 
     conexion.commit()
     conexion.close()
+
+admin_bot.message_loop(gestion)
+
+while 1:
+     time.sleep(100)
