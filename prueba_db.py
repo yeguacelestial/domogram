7c5ae15b224691bfce874a050d5b9f4e65d2336c
Yegua Celestial <rexcolt@localhost.localdomain>
2019-11-02 19:36:10 -0400

    Completed '/agregar' command

--- prueba_db.py
+++ prueba_db.py
@@ -57,96 +57,120 @@ filtro = cursor.fetchall()
 #Print db
 print("ADMINISTRADORES DEL BOT:")
 for a in admins:
-    print ("Nombre: " + a[0] + " - ID: " + str(a[1]) + " - Usuario: @" + str(a[2]))
+    print ("Nombre: " + str(a[0]) + " - ID: " + str(a[1]) + " - Usuario: @" + str(a[2]))
 
 print("\nUSUARIOS: ")
 for u in users:
-    print("Nombre: " + u[0] + " - ID: " + str(u[1]) + " - Usuario: @" + str(u[2]))
+    print("Nombre: " + str(u[0]) + " - ID: " + str(u[1]) + " - Usuario: @" + str(u[2]))
 
 print("\nINTENTOS: ")
 for i in filtro:
-    print("Nombre: " + i[0] + " - ID: " + str(i[1]) + " - Usuario: @" + str(i[2]))
+    print("Nombre: " + str(i[0]) + " - ID: " + str(i[1]) + " - Usuario: @" + str(i[2]))
 
 #Message handler
 def gestor(msg):
     tipo_contenido, tipo_chat, id_chat = telepot.glance(msg)
     nombre = msg['from']['first_name']
     id_user = str(msg['from']['id'])
-    username = msg['from']['username']
 
-    #Grupos
-    if tipo_chat == 'group':
-        userinfo = testbot.getChat(id_chat)
-        print("\nTipo de chat: " + tipo_chat + "\n" + "ID: " + id_user)
+    if 'username' not in msg['from']:
+        username = "No existe"
 
-        if 'username' not in msg['from']:
-            username = "No existe"
-            print("Nombre de usuario: " + username)
+    else:    
+        username = msg['from']['username']
 
-        else:
-            username = str(msg['from']['username'])
-            print("Nombre de usuario: " + username)
+    base = 'testdb.db'
+    conexion = sqlite3.connect(base)
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM usuarios")
+    users = cursor.fetchall()
+    users_list = []
+    for u in users:
+            users_list.append(str(u[2]))
+    
+    if username in users_list:
+        #Grupos
+        if tipo_chat == 'group':
+            userinfo = testbot.getChat(id_chat)
+            print("\nTipo de chat: " + tipo_chat + "\n" + "ID: " + id_user)
 
-        print("Mensaje: " + str(msg['text']) + "\n")
+            if 'username' not in msg['from']:
+                username = "No existe"
+                print("Nombre de usuario: " + username)
+
+            else:
+                username = str(msg['from']['username'])
+                print("Nombre de usuario: " + username)
+
+            print("Mensaje: " + str(msg['text']) + "\n")
+                
+
+        elif tipo_chat == 'private':
+            print("\nTipo de chat: " + tipo_chat + "\n" + "ID: " + id_user)
+            print("Nombre de usuario: " + str(msg['from']['username']))
+            print ("Mensaje: " + msg['text'] + "\n")
+
+        #Si el contenido enviado es texto...
+        if tipo_contenido == 'text':
+            comando = msg['text']
+
+            if comando == '/start':
+                testbot.sendMessage(id_chat, "Bienvenido, " + nombre + "\n"
+                + "Tu ID es: " + id_user + "\n"
+                + "Tu usuario: " + username)
+
+            elif comando.startswith('/agregar '):
+                parametro = msg['text'][9:]
+
+                if parametro.startswith('@'):
+                    testbot.sendMessage(id_chat, "Usuario: " + parametro)
+                    conexion = sqlite3.connect("testdb.db")
+                    cursor = conexion.cursor()
+                    cursor.execute("SELECT (username) FROM usuarios")
+                    users = cursor.fetchall()
+                    users_list = []
+                    for u in users:
+                        users_list.append(str(u[0]))
+
+                    if parametro[1:] in users_list:
+                        testbot.sendMessage(id_chat, parametro + " ya se encuentra registrado.")
+
+                    elif parametro[1:] not in users_list:
+                        cursor.execute("INSERT INTO usuarios (username) VALUES (?)", (parametro[1:], ))
+                        testbot.sendMessage(id_chat, "He registrado a {}".format(parametro))
+                        conexion.commit()
+
+                else:
+                    testbot.sendMessage(id_chat, "Envia un usuario correcto, empezando con @.")
+
             
+            elif comando == '/admins':
+                base = 'testdb.db'
+                conexion = sqlite3.connect(base)
+                cursor = conexion.cursor()
+                cursor.execute("SELECT * FROM admins")
+                admins = cursor.fetchall()
+                for a in admins:
+                    testbot.sendMessage(id_chat, "Nombre: " + a[0] + " - ID: " + str(a[1]) + " - Administrador: @" + a[2])
 
-    elif tipo_chat == 'private':
-        print("\nTipo de chat: " + tipo_chat + "\n" + "ID: " + id_user)
-        print("Nombre de usuario: " + str(msg['from']['username']))
-        print ("Mensaje: " + msg['text'] + "\n")
+            elif comando == '/usuarios':
+                base = 'testdb.db'
+                conexion = sqlite3.connect(base)
+                cursor = conexion.cursor()
+                cursor.execute("SELECT * FROM usuarios")
+                users = cursor.fetchall()
+                testbot.sendMessage(id_chat, "Usuarios del bot:")
+                for u in users:
+                    if str(u[0]) != 'None' or str(u[1]) != 'None':
+                        testbot.sendMessage(id_chat, "Nombre: " + str(u[0]) + "\nID: " + str(u[1]) + "\nUsuario: @" + u[2])
+                    else:
+                        testbot.sendMessage(id_chat, "Nombre: Sin especificar\nID: Sin especificar\nUsuario: @" + u[2])
 
-    #Si el contenido enviado es texto...
-    if tipo_contenido == 'text':
-        comando = msg['text']
-
-        if comando == '/start':
-            testbot.sendMessage(id_chat, "Bienvenido, " + nombre + "\n"
-            + "Tu ID es: " + id_user + "\n"
-            + "Tu usuario: " + username)
-
-        elif comando == '/guardar':
-            testbot.sendMessage(id_chat, "Hola, " + nombre)
-            conexion = sqlite3.connect("testdb.db")
-            cursor = conexion.cursor()
-            cursor.execute("SELECT * FROM usuarios")
-            users = cursor.fetchall()
-            users_list = []
-
-            for u in users:
-                users_list.append(str(u[1]))
-
-            if id_user not in users_list:
-                cursor.execute("INSERT INTO usuarios (nombre, id, username) VALUES (?, ?, ?)", (nombre, id_user, username, ))
-                testbot.sendMessage(id_chat, "Datos guardados en la base de datos.")
-                conexion.commit()
-                conexion.close()
-
-            elif id_user in users_list:
-                testbot.sendMessage(id_chat, "Usuario ya registrado.")
-
-        
-        elif comando == '/fetch_admins':
-            base = 'testdb.db'
-            conexion = sqlite3.connect(base)
-            cursor = conexion.cursor()
-            cursor.execute("SELECT * FROM admins")
-            admins = cursor.fetchall()
-            testbot.sendMessage(id_chat, "ADMINS:")
-            for a in admins:
-                testbot.sendMessage(id_chat, "Nombre: " + a[0] + " - ID: " + str(a[1]) + " - Administrador: @" + a[2])
-
-        elif comando == '/fetch_users':
-            base = 'testdb.db'
-            conexion = sqlite3.connect(base)
-            cursor = conexion.cursor()
-            cursor.execute("SELECT * FROM usuarios")
-            users = cursor.fetchall()
-            testbot.sendMessage(id_chat, "Usuarios del bot:")
-            for u in users:
-                testbot.sendMessage(id_chat, "Nombre: " + u[0] + " - ID: " + str(u[1]) + " - Usuario: @" + u[2])
-
-        else:
-            testbot.sendMessage(id_chat, "No entendi, " + nombre)
+            else:
+                testbot.sendMessage(id_chat, "No entendi, " + nombre)
+    
+    else:
+        testbot.sendMessage(id_chat, "Acceso denegado.")
 
 #Loop
 testbot.message_loop(gestor)
