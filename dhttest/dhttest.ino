64a58391afae7b4824f9c2b82e018e83caedda8d
Yegua Celestial <rexcolt@localhost.localdomain>
2019-11-13 17:02:35 -0500

    Temperature and humidity test done, Arduino decided to stop fucking with DHT11

---
+++ dhttest/dhttest.ino
@@ -0,0 +1,25 @@
+#include <DHT.h>
+
+#define DHT_PIN A0
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_PIN, DHT_TYPE);
+void setup(){
+    Serial.begin(9600);
+    delay(500);
+    Serial.println("DHT11 Humedad y Temperatura\n\n");
+    delay(1000);
+    dht.begin();
+}
+
+void loop(){
+
+    Serial.print("Temperatura: ");
+    Serial.print(dht.readTemperature());
+    Serial.println("°C");
+    Serial.print("Humedad: ");
+    Serial.print(dht.readHumidity());
+    Serial.print("%");
+    Serial.println("\n");
+    delay(2000);
+}
